896c85ac38c09e34f6846b17a547e50423c6bf84
Phuc Tran <glup3.tran@gmail.com>
2019-11-22 16:26:41 +0100

    add active in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-dark mb-4">
-      <Link to="/" className="navbar-brand">
+      <Link to="/" className="navbar-brand active">
         Spenger Survival
       </Link>
+
       <button
         className="navbar-toggler"
         type="button"
@@ -21,29 +24,29 @@ const Navbar = () => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
+          <li className={`nav-item ${pathname === '/about' ? 'active' : ''}`}>
             <Link to="/about" className="nav-link">
               About
             </Link>
           </li>
-          <li className="nav-item">
+          <li className={`nav-item ${pathname === '/tipp-adden' ? 'active' : ''}`}>
             <Link to="/tipp-adden" className="nav-link">
               Tipp abgeben
             </Link>
           </li>
-          <li className="nav-item">
+          <li className={`nav-item ${pathname === '/hall-of-fame' ? 'active' : ''}`}>
             <Link to="/hall-of-fame" className="nav-link">
               Hall of Fame
             </Link>
           </li>
         </ul>
         <ul className="navbar-nav">
-          <li className="nav-item">
+          <li className={`nav-item ${pathname === '/datenschutz' ? 'active' : ''}`}>
             <Link to="/datenschutz" className="nav-link">
               Datenschutzerklärung
             </Link>
           </li>
-          <li className="nav-item">
+          <li className={`nav-item ${pathname === '/impressum' ? 'active' : ''}`}>
             <Link to="/impressum" className="nav-link">
               Impressum
             </Link>
