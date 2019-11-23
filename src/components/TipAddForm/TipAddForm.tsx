301df7413ee714725865641d050f5a5192c4f4a6
Phuc Tran <glup3.tran@gmail.com>
2019-11-23 01:04:49 +0100

    add state to form

--- src/components/TipAddForm/TipAddForm.tsx
+++ src/components/TipAddForm/TipAddForm.tsx
@@ -3,17 +3,33 @@ import ReactQuill from 'react-quill';
 import ReCAPTCHA from 'react-google-recaptcha';
 
 import './TipAddForm.scss';
+import useInput from '../../hooks/input-hook';
 
 const TipAddForm = () => {
-  const [captchaValue, setCaptchaValue] = useState(null);
+  const { value: nameValue, bind: nameBind } = useInput('');
+  const { value: schoolClassValue, bind: schoolClassBind } = useInput('');
+  const { value: titleValue, bind: titleBind } = useInput('');
+  const { value: genderValue, bind: genderBind } = useInput('');
+  const { value: departmentValue, bind: departmentBind } = useInput('');
   const [description, setDescription] = useState('');
+
+  const [captchaValue, setCaptchaValue] = useState(null);
   const [limitExceeded, setLimitExceeded] = useState(false);
 
   const onSubmit = e => {
     e.preventDefault();
 
-    console.log('cool form', description);
-    console.log('captcha', captchaValue);
+    const newTip = {
+      title: titleValue,
+      schoolClass: schoolClassValue === '' ? null : schoolClassValue,
+      author: nameValue === '' ? null : nameValue,
+      gender: genderValue === '' ? null : genderValue,
+      department: departmentValue === '' ? null : departmentValue,
+      captcha: captchaValue,
+      description,
+    };
+
+    console.log('NEW TIP', newTip);
   };
 
   const onDescriptionChange = (content, _, __, editor) => {
@@ -30,66 +46,64 @@ const TipAddForm = () => {
     <form onSubmit={onSubmit}>
       <div className="row mb-4">
         <div className="col">
-          <div>
-            <label htmlFor="name" className="float-left">
-              Name
-            </label>
-            <div className="form-check form-check-inline float-right mr-0">
-              <input className="form-check-input" type="checkbox" id="checkboxName" value="anonym" />
-              <label className="form-check-label" htmlFor="checkboxName">
-                Anonym
-              </label>
-            </div>
-          </div>
-          <input id="name" type="text" className="form-control" placeholder="Name" />
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            className="form-control"
+            placeholder="Name leer lassen --> Anonym"
+            value={nameValue}
+            {...nameBind}
+          />
         </div>
 
         <div className="col">
           <label htmlFor="geschlecht">Geschlecht</label>
-          <select className="form-control" id="geschlecht">
-            <option>Keine Angabe</option>
-            <option>Männlich</option>
-            <option>Weiblich</option>
+          <select className="form-control" id="geschlecht" value={genderValue} {...genderBind}>
+            <option value="">Keine Angabe</option>
+            <option value="m">Männlich</option>
+            <option value="w">Weiblich</option>
           </select>
         </div>
       </div>
 
       <div className="row mb-4">
         <div className="col">
-          <div>
-            <label htmlFor="klasse" className="float-left">
-              Klasse
-            </label>
-            <div className="form-check form-check-inline float-right mr-0">
-              <input className="form-check-input" type="checkbox" id="checkboxKlasse" value="anonym" />
-              <label className="form-check-label" htmlFor="checkboxKlasse">
-                Anonym
-              </label>
-            </div>
-          </div>
-          <input id="klasse" type="text" className="form-control" placeholder="Klasse" />
+          <label htmlFor="klasse">Klasse</label>
+          <input
+            id="klasse"
+            type="text"
+            className="form-control"
+            placeholder="Klasse leer lassen --> Anonym"
+            value={schoolClassValue}
+            {...schoolClassBind}
+          />
         </div>
 
         <div className="col">
           <label htmlFor="abteilung">Abteilung</label>
-          <select className="form-control" id="abteilung">
-            <option>Unbekannt</option>
-            <option>Informatik</option>
-            <option>Kunst und Design</option>
-            <option>Biomedizin</option>
-            <option>Gamedesign</option>
+          <select className="form-control" id="abteilung" value={departmentValue} {...departmentBind}>
+            <option value="">Unbekannt</option>
+            <option value="Animation">Animation</option>
+            <option value="Betriebsinformatik">Betriebsinformatik</option>
+            <option value="Biomedizin">Biomedizin</option>
+            <option value="Fachschule Informatik">Fachschule Informatik</option>
+            <option value="Gamedesign">Gamedesign</option>
+            <option value="Interior- und Surfacedesign">Interior- und Surfacedesign</option>
+            <option value="Informatik">Informatik</option>
+            <option value="Wirtschaft">Wirtschaft</option>
           </select>
         </div>
       </div>
 
       <div className="form-group">
         <label htmlFor="titel">Titel</label>
-        <input type="text" className="form-control" id="titel" placeholder="Titel" />
+        <input type="text" className="form-control" id="titel" placeholder="Titel" value={titleValue} {...titleBind} />
       </div>
 
       <div className="form-group">
-        <label htmlFor="beschreibung">Beschreibung</label>
-        <ReactQuill defaultValue={description} onChange={onDescriptionChange} />
+        <label htmlFor="beschreibung">Text</label>
+        <ReactQuill defaultValue={description} onChange={onDescriptionChange} id="beschreibung" />
         {limitExceeded ? <span className="float-right text-danger">Zu viele Zeichen!</span> : <></>}
       </div>
 
