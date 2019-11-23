30cdcf2b0da775ad5362664dff54cc0ede204a3e
Phuc Tran <glup3.tran@gmail.com>
2019-11-23 20:14:23 +0100

    add person type

--- src/pages/HallOfFamePage.tsx
+++ src/pages/HallOfFamePage.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import Person from '../types/person';
+
+const developers: Person[] = [
+  {
+    name: 'Phuc Tran',
+    title: 'Admin & Owner',
+    email: 'glup3.tran@gmail.com',
+  },
+  {
+    name: 'Glup3',
+    title: 'Frontend & Backend',
+    email: 'glup3.tran@gmail.com',
+  },
+];
+
+const helpers: Person[] = [
+  {
+    name: 'Max G',
+    title: 'Datenschutzexperte',
+    instagram: 'instagram.com',
+  },
+  {
+    name: 'Dave',
+    title: 'UX Specialist',
+    snapchat: 'snapchat.com',
+  },
+  {
+    name: 'Lena',
+    title: 'Marketing Queen',
+    twitter: 'twitter.com',
+  },
+];
 
 const HallOfFamePage = () => {
   return (
-    <div>
-      <h1>Hall of Fame</h1>
+    <div className="container">
+      <h1 className="text-center">Hall of Fame</h1>
+
+      <h2>Developers</h2>
+      {developers.map((person, index) => (
+        <p key={`dev-${index}`}>
+          {person.title} {person.name}
+        </p>
+      ))}
+
+      <h2>Helpers</h2>
+      {helpers.map((person, index) => (
+        <p key={`dev-${index}`}>{person.name}</p>
+      ))}
     </div>
   );
 };
---
+++ src/types/person.d.ts
@@ -0,0 +1,14 @@
+export default interface Person {
+  name: string;
+  image?: string;
+  title?: string;
+  email?: string;
+  instagram?: string;
+  twitter?: string;
+  linkedin?: string;
+  snapchat?: string;
+  facebook?: string;
+  discord?: string;
+  paypal?: string;
+  youtube?: string;
+}
