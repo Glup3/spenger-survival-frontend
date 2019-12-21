db9f2ed222a1bdbaec76a2fb39bcf1e69767c29b
Phuc Tran <glup3.tran@gmail.com>
2019-12-21 19:40:00 +0100

    change to dropdown select add category as feedback

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAlert } from 'react-alert';
+import DropdownSelect from 'react-dropdown-select';
 
 import useInput from '../../hooks/input-hook';
 import RequiredStar from '../RequiredStar';
 import { useData } from '../../context/dataContext';
 
+const feedbackOptions: DropdownSelectOption[] = [
+  { label: 'Feedback geben', value: 'feedback' },
+  { label: 'Feature hinzufügen / ändern / löschen / ...', value: 'feature' },
+  { label: 'Kategorie hinzufügen / ändern / löschen / ...', value: 'category' },
+  { label: 'Bug melden', value: 'bug' },
+  { label: 'Sonstiges', value: null },
+];
+
 const FeedbackForm = () => {
   const data = useData();
   const alert = useAlert();
 
+  const [feedbackType, setFeedbackType] = useState<DropdownSelectOption>(feedbackOptions[0]);
   const { value: messageValue, bind: messageBind, reset: messageReset } = useInput('');
-  const { value: typeValue, bind: typeBind } = useInput('feedback');
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    data.sendFeedback(messageValue, typeValue);
+    data.sendFeedback(messageValue, feedbackType.value);
     alert.show('Feedback wurde gesendet. Danke!');
     messageReset();
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <div className="form-group">
-        <label htmlFor="messageType">
-          Art
-          <RequiredStar />
-        </label>
-        <select className="form-control" id="messageType" value={typeValue} {...typeBind} required>
-          <option value="feedback">Feedback</option>
-          <option value="feature">Neues Feature</option>
-          <option value="bug">Bug</option>
-          <option value="">Sonstiges</option>
-        </select>
+      <div className="form-group row">
+        <div className="col-6">
+          <label htmlFor="messageType">
+            Art
+            <RequiredStar />
+          </label>
+          <DropdownSelect
+            id="messageType"
+            searchable={false}
+            closeOnScroll={true}
+            options={feedbackOptions}
+            values={[feedbackType]}
+            onChange={(values: DropdownSelectOption[]) => setFeedbackType(values[0])}
+          />
+        </div>
       </div>
 
       <div className="form-group">
